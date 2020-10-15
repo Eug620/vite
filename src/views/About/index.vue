e23fdfa9f7656be5de5656faf5f4aa3dcff46824
yeyuhang <yeyuhang@appicplay.com>
2020-10-15 20:09:40 +0800

    update

---
+++ src/views/About/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: yeyuhang
+ * @Date: 2020-10-15 19:18:02
+ * @LastEditTime: 2020-10-15 19:19:26
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+-->
+<template>
+  <div>about</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: yeyuhang
+ * @Date: 2020-10-15 19:17:48
+ * @LastEditTime: 2020-10-15 19:19:34
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+-->
+<template>
+  <div>home</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
