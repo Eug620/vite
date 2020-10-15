913dfd2c81050329a7d09327a1ff23d0338ffb3a
yeyuhang <yeyuhang@appicplay.com>
2020-10-15 16:39:54 +0800

    vite-app init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{refss.a}}</p>
+    <textarea  v-model="refss.a">
+    <button :refs="refs">按钮</button>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, onMounted } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup (_, ctx) {
+    const refss = reactive({ a: 'hello'})
+    const refs = ref(null)
+    onMounted(() => {
+      console.log('// mounted取到ref模版')
+      console.log(refs)
+    })
+    return {
+      refss,
+      refs
+    }
+  }
+}
+</script>
